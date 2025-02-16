2e676e95417d749091a3bcadcc7137f0a0aa0d10
Nandhagopiv <nandhagopy@gmail.com>
2025-02-17 00:47:23 +0530

    Started a webpage Jaisai

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import Home from "./components/Home"
-import {Route, Routes } from 'react-router-dom'
-import Menu from "./components/Menu"
+import { Route, Routes } from 'react-router-dom'
 import { Fragment } from "react"
+import About from "./components/About"
+import Client from "./components/Client"
+import Jobseeker from "./components/Jobseeker"
+import Contact from "./components/Contact"
+import Service from "./components/Service"
 
 const App = () => {
   return (
     <Fragment>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/menu" element={<Menu />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/clients" element={<Client />}></Route>
+        <Route path="/services" element={<Service/>}></Route>
+        <Route path="/jobseekers" element={<Jobseeker />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </Fragment>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,55 +7,67 @@ const Nav = (props) => {
 
     const [sideNav, setSideNav] = useState(false)
 
-    const handleMenu = () => {
-        Navigate('/menu')
+    const handleAbout = () => {
+        Navigate('/about')
+    }
+
+    const handleService = () => {
+        Navigate('/services')
+    }
+
+    const handleSeekers = () => {
+        Navigate('/jobseekers')
     }
 
     const handleHome = () => {
         Navigate('/')
     }
 
-    const handleSign = () => {
-        Navigate('/menu')
+    const handleClient = () => {
+        Navigate('/clients')
     }
 
-    const handleCart = () => {
-        Navigate('/')
+    const handleContact = () => {
+        Navigate('/contact')
     }
 
-    const handleCloseNav = () =>{
+    const handleCloseNav = () => {
         setSideNav(false)
     }
 
-    const handleOpenNav =() =>{
+    const handleOpenNav = () => {
         setSideNav(true)
     }
     return (
         <Fragment>
-            <main className="w-[100%] bg-[#0E1317] text-white lg:px-24 p-8 lg:py-8 sticky top-0 z-10 flex items-center justify-between">
-                <section className="flex lg:flex-row flex-col items-center font-bold lg:gap-2">
-                    <h2 className="lg:text-3xl text-2xl">Ambur Biriyani</h2>
-                    <h2 className="lg:text-3xl text-2xl text-[#E13750]">Kallakurichi</h2>
+            <main className="w-[100%] shadow bg-white text-white lg:px-24 p-8 lg:py-8 sticky top-0 z-20 flex items-center justify-between">
+                <section className="flex lg:flex-row flex-col text-[#EC3A54] items-center font-bold lg:gap-2">
+                    <h2 className="lg:text-4xl text-2xl">JAISAI</h2>
+                    <h2 className="lg:text-4xl text-2xl">Academy</h2>
                 </section>
-                <section className="lg:flex hidden font-bold gap-10 cursor-pointer">
-                    <p style={{ color: props.home ? '#E13750' : '' }} onClick={handleHome} className="hover:text-[#E13750]">HOME</p>
-                    <p style={{ color: props.menu ? '#E13750' : '' }} onClick={handleMenu} className="hover:text-[#E13750]">MENU</p>
-                    <p style={{ color: props.sign ? '#E13750' : '' }} onClick={handleSign} className="hover:text-[#E13750]">LOGIN IN/SIGN UP</p>
-                    <p style={{ color: props.cart ? '#E13750' : '' }} onClick={handleCart} className="hover:text-[#E13750]">CART</p>
+                <section className="lg:flex hidden font-bold text-black gap-10 cursor-pointer">
+                    <p style={{ color: props.home ? '#EC3A54' : '' }} onClick={handleHome} className="hover:text-[#EC3A54]">HOME</p>
+                    <p style={{ color: props.course ? '#EC3A54' : '' }} onClick={handleAbout} className="hover:text-[#EC3A54]">ABOUT US</p>
+                    <p style={{ color: props.client ? '#EC3A54' : '' }} onClick={handleClient} className="hover:text-[#EC3A54]">CLIENTS</p>
+                    <p style={{ color: props.service ? '#EC3A54' : '' }} onClick={handleService} className="hover:text-[#EC3A54]">SERVICES</p>
+                    <p style={{ color: props.seekers ? '#EC3A54' : '' }} onClick={handleSeekers} className="hover:text-[#EC3A54]">JOB SEEKERS</p>
+                    <p style={{ color: props.contact ? '#EC3A54' : '' }} onClick={handleContact} className="hover:text-[#EC3A54]">CONTACT US</p>
                 </section>
                 <section className="lg:hidden flex">
                     <img onClick={handleOpenNav} className="w-10" src={menuIcon} alt="menu" />
                 </section>
             </main>
-            <section style={{right: sideNav?'0':'-75%'}} className="fixed w-[75%] bg-[#FAF7F2] duration-500 p-10 top-0 -right-[75%] z-20 h-[100%]">
-                    <div className="flex flex-col font-bold gap-10">
-                        <p onClick={handleCloseNav} className="text-right mb-5 text-3xl">x</p>
-                        <p style={{ color: props.home ? '#E13750' : '' }} onClick={handleHome} className="hover:text-[#E13750]">HOME</p>
-                        <p style={{ color: props.menu ? '#E13750' : '' }} onClick={handleMenu} className="hover:text-[#E13750]">MENU</p>
-                        <p style={{ color: props.sign ? '#E13750' : '' }} onClick={handleSign} className="hover:text-[#E13750]">LOGIN IN/SIGN UP</p>
-                        <p style={{ color: props.cart ? '#E13750' : '' }} onClick={handleCart} className="hover:text-[#E13750]">CART</p>
-                    </div>
-                </section>
+            <section style={{ right: sideNav ? '0' : '-75%' }} className="fixed w-[75%] bg-gray-100 duration-500 p-10 top-0 -right-[75%] z-30 h-[100%]">
+                <div className="flex flex-col font-bold text-black gap-10">
+                    <p onClick={handleCloseNav} className="text-right mb-5 text-3xl">x</p>
+                    <p style={{ color: props.home ? '#EC3A54' : '' }} onClick={handleHome} className="hover:text-[#EC3A54]">HOME</p>
+                    <p style={{ color: props.course ? '#EC3A54' : '' }} onClick={handleAbout} className="hover:text-[#EC3A54]">ABOUT US</p>
+                    <p style={{ color: props.client ? '#EC3A54' : '' }} onClick={handleClient} className="hover:text-[#EC3A54]">CLIENTS</p>
+                    <p style={{ color: props.service ? '#EC3A54' : '' }} onClick={handleService} className="hover:text-[#EC3A54]">SERVICES</p>
+                    <p style={{ color: props.seekers ? '#EC3A54' : '' }} onClick={handleSeekers} className="hover:text-[#EC3A54]">JOB SEEKERS</p>
+                    <p style={{ color: props.contact ? '#EC3A54' : '' }} onClick={handleContact} className="hover:text-[#EC3A54]">CONTACT US</p>
+                </div>
+            </section>
         </Fragment>
     )
 }
